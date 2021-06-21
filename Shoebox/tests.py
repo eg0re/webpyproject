be771605fcce92a5c7975d64e9fa44e49168a80c
georg <noerig@pm.me>
2021-06-21 15:50:05 +0200

    added Useradmin based on lecture 3

--- Shoebox/tests.py
+++ Shoebox/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from djanconf defined in webpyproject.urls, Django tried these URL patterns, in this order:
+
+    admin/
+    ^media/(?P<path>.*)$
+
+The empty path didn’t match any of these.
+
+You’re seeing this error because you have DEBUG = True in your Django settings file. Change that to False, and Django will display a standard 404 page.
+go.test import TestCase
 
 # Create your tests here.
--- webpyproject/settings.py
+++ webpyproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Shoebox.apps.ShoeboxConfig',
+    'Useradmin.apps.UseradminConfig',
 ]
 
 MIDDLEWARE = [
@@ -130,6 +131,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
