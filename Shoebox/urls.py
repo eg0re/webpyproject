5634c456042b2a54d9457a51ab44f07eb3f8e2dd
georg <noerig@pm.me>
2021-07-06 19:27:18 +0200

    added detailview and basic submit functionality

--- Shoebox/urls.py
+++ Shoebox/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ShoeboxListView.as_view(), name='box-list'),
-    path('show/<int:pk>/', views.ShoeboxDetailView.as_view(), name='box-detail'),
+    path('box/<int:pk>/', views.ShoeboxDetailView.as_view(), name='box-detail'),
+    path('add/', views.ShoeboxCreateView.as_view(), name="box-create"),
 ]
